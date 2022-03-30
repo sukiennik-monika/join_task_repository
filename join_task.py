07127f6b01a695eeb0accb516a7b7579bf1e4f10
sukiennik-monika <62392645+sukiennik-monika@users.noreply.github.com>
2022-03-30 18:44:38 +0200

    Update join_task.py

--- join_task.py
+++ join_task.py
@@ -1,9 +1,11 @@
-from typing import Union
+import os
+from typing import Union, List, Tuple
 import pandas as pd
 import numpy as np
 
+
 class CSVFile():
-    def __init__(self, path_or_file : Union[pd.DataFrame, str]):
+    def __init__(self, path_or_file: Union[pd.DataFrame, str]):
         self.path_or_file = path_or_file
         if isinstance(self.path_or_file, str):
             self._is_right_path()
@@ -22,82 +24,73 @@ class CSVFile():
         except FileNotFoundError:
             print('File not found on this path')
 
-def joing_function(output: pd.DataFrame, taken_from_right_file: pd.DataFrame, column_name: str):
-    taken_from_right_file = taken_from_right_file[[col for col in taken_from_right_file.columns if col != column_name]]
-    output = output.sort_values(by=[column_name])
-    cnt = len(output.columns.values)
-    for col, data in taken_from_right_file.items():
-        output.insert(cnt, col, data)
-        cnt += 1
-    output_csv = CSVFile(output)
-    print(output_csv)
 
-def common_val_search(left_file : pd.DataFrame, right_file : pd.DataFrame, column_name : str):
+def common_val_search(left_file: pd.DataFrame, right_file: pd.DataFrame, column_name: str) -> List:
     common_values = []
     for i, row in left_file.iterrows():
         if row[column_name] in right_file[column_name].unique():
             common_values.append(row[column_name])
     return common_values
 
-def inner_join(left_file : pd.DataFrame, right_file : pd.DataFrame, column_name : str):
-    output = pd.DataFrame()
-    common_values = common_val_search(left_file, right_file, column_name)
-    print(common_values)
-    for i, row in left_file.iterrows():
-        if row[column_name] in common_values:
-            output = output.append(row, ignore_index=True)
-    taken_from_right_file = pd.DataFrame()
-    for i, row in right_file.iterrows():
-        if row[column_name] in common_values:
-            taken_from_right_file = taken_from_right_file.append(row, ignore_index=True)
-    joing_function(output,taken_from_right_file, column_name)
 
-
-
-def insert_row(df, row_number, row_value):
+def insert_row_between_others(dataframe, row_number, row_value) -> pd.DataFrame:
     start_upper = 0
     end_upper = row_number
     start_lower = row_number
-    end_lower = df.shape[0]
+    end_lower = dataframe.shape[0]
     upper_half = [*range(start_upper, end_upper, 1)]
     lower_half = [*range(start_lower, end_lower, 1)]
     lower_half = [x.__add__(1) for x in lower_half]
     index_ = upper_half + lower_half
-    df.index = index_
-    df.loc[row_number] = row_value
-    df = df.sort_index()
-    return df
+    dataframe.index = index_
+    dataframe.loc[row_number] = row_value
+    return dataframe.sort_index()
 
-def left_or_right_join(left_file : pd.DataFrame, right_file : pd.DataFrame, column_name : str, join_type : str):
+
+def inner_join(left_file: pd.DataFrame, right_file: pd.DataFrame, column_name: str) -> Tuple[
+    pd.DataFrame, pd.DataFrame]:
+    proper_file = pd.DataFrame()
     common_values = common_val_search(left_file, right_file, column_name)
-    left_file = left_file.sort_values(by=[column_name],ignore_index=True)
-    right_file = right_file.sort_values(by=[column_name],ignore_index=True)
-    if join_type == 'left'.casefold():
-        output = left_file[:]
-        opposite_file = right_file[:]
-    else:
-        output = right_file[:]
-        opposite_file = left_file[:]
-    taken_from_opposite_file_and_rest = pd.DataFrame()
+    for i, row in left_file.iterrows():
+        if row[column_name] in common_values:
+            proper_file = proper_file.append(row, ignore_index=True)
+    taken_from_right_file = pd.DataFrame()
+    for i, row in right_file.iterrows():
+        if row[column_name] in common_values:
+            taken_from_right_file = taken_from_right_file.append(row, ignore_index=True)
+    return proper_file, taken_from_right_file
+
+
+def left_or_right_join(proper_file: pd.DataFrame, opposite_file: pd.DataFrame, column_name: str)-> Tuple[
+    pd.DataFrame, pd.DataFrame]:
+    common_values = common_val_search(proper_file, opposite_file, column_name)
+    proper_file = proper_file.sort_values(by=[column_name], ignore_index=True)
+    opposite_file = opposite_file.sort_values(by=[column_name], ignore_index=True)
+    taken_from_opposite_file = pd.DataFrame()
     not_in_opposite_file_ids = []
-    for i, row in output.iterrows():
+    for i, row in proper_file.iterrows():
         if row[column_name] not in common_values:
             not_in_opposite_file_ids.append(i)
     for i, row in opposite_file.iterrows():
         if row[column_name] in common_values:
-            taken_from_opposite_file_and_rest = taken_from_opposite_file_and_rest.append(row, ignore_index=True)
+            taken_from_opposite_file = taken_from_opposite_file.append(row, ignore_index=True)
     for i in not_in_opposite_file_ids:
-            taken_from_opposite_file_and_rest = insert_row(taken_from_opposite_file_and_rest, i, [np.NaN for j in range(len(opposite_file.columns))])
-    joing_function(output, taken_from_opposite_file_and_rest, column_name)
+        taken_from_opposite_file = insert_row_between_others(taken_from_opposite_file, i,
+                                                             [np.nan for j in range(len(opposite_file.columns))])
+    return proper_file, taken_from_opposite_file
 
 
+def final_joining(output: pd.DataFrame, taken_from_right_file: pd.DataFrame, column_name: str) -> pd.DataFrame:
+    taken_from_right_file = taken_from_right_file[[col for col in taken_from_right_file.columns if col != column_name]]
+    output = output.sort_values(by=[column_name])
+    cnt = len(output.columns.values)
+    for col, data in taken_from_right_file.items():
+        output.insert(cnt, col, data)
+        cnt += 1
+    return output
 
 
-
-
-
-
-def join_method(left_file_path: str, right_file_path: str, column_name: str, join_type: str):
+def join(left_file_path: str, right_file_path: str, column_name: str, join_type: str):
     left_file = CSVFile(left_file_path)
     right_file = CSVFile(right_file_path)
     column_name = column_name.upper()
@@ -108,15 +101,18 @@ def join_method(left_file_path: str, right_file_path: str, column_name: str, joi
                                                                                              col_name in
                                                                                              right_file.csv_file.columns]:
         if join_type.casefold() == "inner".casefold():
-            inner_join(left_file.csv_file, right_file.csv_file, column_name)
-        elif join_type.casefold() == "left".casefold() or join_type.casefold() == "right".casefold():
-            left_or_right_join(left_file.csv_file, right_file.csv_file, column_name, join_type)
+            proper_file, taken_from_opposite_file = inner_join(left_file.csv_file, right_file.csv_file, column_name)
+        elif join_type.casefold() == "left".casefold():
+            proper_file, taken_from_opposite_file = left_or_right_join(left_file.csv_file, right_file.csv_file,
+                                                                       column_name)
+        elif join_type.casefold() == "right".casefold():
+            proper_file, taken_from_opposite_file = left_or_right_join(right_file.csv_file, left_file.csv_file,
+                                                                       column_name)
         else:
             raise ValueError('There are 3 types of joining: inner, left, right')
     else:
         raise ValueError('No column with given column_name')
+    output = final_joining(proper_file, taken_from_opposite_file, column_name)
+    print(output)
+    output.to_csv(os.path.dirname(os.path.abspath(left_file_path)) + '//joining_result.csv',index=False)
 
-
-if __name__ == "__main__":
-    join_method('D:\\pycharm\\join_task_test\\product.csv', 'D:\\pycharm\\join_task_test\\customer.csv', 'Product_id',
-                'right')
